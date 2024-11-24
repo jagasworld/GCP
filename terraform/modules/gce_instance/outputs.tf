cbf27e9e97d338d95ef7466c93f6b56a74a97b99
Jag S <42541369+jagasworld@users.noreply.github.com>
2024-11-24 18:58:15 +0530

    Update outputs.tf

--- terraform/modules/gce_instance/outputs.tf
+++ terraform/modules/gce_instance/outputs.tf
@@ -12,3 +12,44 @@ output "instance_external_ip" {
   description = "The external IP address of the created instance"
   value       = google_compute_instance.default.network_interface[0].access_config[0].nat_ip
 }
+
+output "id" {
+  value = data.google_compute_image.my_image.id
+}
+
+
+output "self_link" {
+  value = data.google_compute_image.my_image.self_link
+}
+
+output "name" {
+  value = data.google_compute_image.my_image.name
+}
+
+output "family" {
+  value = data.google_compute_image.my_image.family
+}
+
+output "image_id" {
+  value = data.google_compute_image.my_image.image_id
+}
+
+output "status" {
+  value = data.google_compute_image.my_image.status
+}
+
+output "licenses" {
+  value = data.google_compute_image.my_image.licenses
+}
+
+output "description" {
+  value = data.google_compute_image.my_image.description
+}
+
+output "project" {
+  value = data.google_compute_image.my_image.project
+}
+
+output "source_image_id" {
+  value = data.google_compute_image.my_image.source_image_id
+}
